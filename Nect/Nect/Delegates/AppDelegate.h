e52e092e15c3e5e113815d72b283fb0f34bf4715
alyssatly <alyssatan2010@gmail.com>
2020-07-13 13:43:42 -0700

    User can sign up and log in

---
+++ Nect/Nect/Delegates/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  Nect
+//
+//  Created by Alyssa Tan on 7/13/20.
+//  Copyright © 2020 Alyssa Tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+
+@end
+
---
+++ Nect/Nect/Delegates/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Nect
+//
+//  Created by Alyssa Tan on 7/13/20.
+//  Copyright © 2020 Alyssa Tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
